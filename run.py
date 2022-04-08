043a286cf7fdc95bc24d185dfc232e6cb2f51cd4
eoghankb <eoghankb@gmail.com>
2022-04-08 00:18:16 +0000

    add function ship positions

--- run.py
+++ run.py
@@ -23,3 +23,35 @@ def checkForHit(row, col):
     return False
 
 computer_grid = generateGrid()
+
+def shipPosition():
+    for ship in range(0,ships):
+        ship_size = random.randint(1, 4)
+        direction = random.randint(1, 4)
+        rowstart = random.randint(0, len(computer_grid))
+        colstart = random.randint(0, len(computer_grid))
+        for i in range(0,ship_size):
+            if rowstart +1 > len(computer_grid) or rowstart -1 < 0 or colstart +1 > len(computer_grid) or colstart -1 < 0:
+                computer_grid[rowstart][colstart] = "."
+                rowstart = random.randint(0, len(computer_grid))
+                colstart = random.randint(0, len(computer_grid))
+            if direction == 1:
+                if not computer_grid[rowstart -1][colstart] == "0":
+                    computer_grid[rowstart -1][colstart] = "0"
+                    rowstart = rowstart -1
+            if direction == 2:
+                if not computer_grid[rowstart][colstart +1] == "0":
+                    computer_grid[rowstart][colstart +1] = "0"
+                    colstart = colstart +1
+            if direction == 3:
+                if not computer_grid[rowstart +1][colstart] == "0":
+                    computer_grid[rowstart +1][colstart] = "0"
+                    rowstart = rowstart +1
+            if direction == 4: 
+                if not computer_grid[rowstart][colstart -1] == "0":
+                    computer_grid[rowstart][colstart -1] = "0"
+                    colstart = colstart -1
+        computer_grid[rowstart][colstart] = "0"
+player_grid = generateGrid()
+
+shipPosition()

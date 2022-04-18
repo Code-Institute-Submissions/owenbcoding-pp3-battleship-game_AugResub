f603935d5af2e713e4139fe5f5d405c33e08adca
eoghankb <eoghankb@gmail.com>
2022-04-18 19:51:19 +0000

    Add Ships hidden for players to guess on grid

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
 # To - do
 # The ability for the user to set the grid size
 
-board = []
-for x in range(5):
-    board.append("'0','0','0','0','0'")
+from random import randint
 
-print(board)
+board = []
+
+for i in range(0, 5):
+    board.append(['o'] * 5)
+
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+
+def rand_row(board_in):
+    return randint(0, len(board_in) - 1)
+
+
+def random_col(board_in):
+    return randint(0, len(board_in) - 1)
+
+
+ship_row = random_row(board)
+ship_col = random_col(board)

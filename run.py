41a9b333b0a7e04b7936d26a77efdac64e56817d
eoghankb <eoghankb@gmail.com>
2022-08-28 05:50:57 +0000

    Added Function print_board

--- run.py
+++ run.py
@@ -5,5 +5,11 @@ HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 # Board for displaying hits and misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)
 
-
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
